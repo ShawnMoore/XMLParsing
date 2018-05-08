94e881b6897007f4cf8f09c00037e72b57b17cb2
Ondrej Rafaj <ondrej.rafaj@gmail.com>
2018-05-08 21:22:42 +0100

    wip

--- Tests/XMLCodingTests/XMLParsingTests.swift
+++ Tests/XMLCodingTests/XMLParsingTests.swift
@@ -1,12 +1,13 @@
 import XCTest
-@testable import XMLParsing
+@testable import XMLCoding
+
 
 class XMLParsingTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(XMLParsing().text, "Hello, World!")
+//        XCTAssertEqual(XMLParsing().text, "Hello, World!")
     }
 
 
